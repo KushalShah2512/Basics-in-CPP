c449249ae4d356fe78c9df8577ed2492c5a24412
Kushal Shah <124665390+KushalShah2512@users.noreply.github.com>
2026-01-13 11:25:46 +0530

    EvenOdd program to check number parity

---
+++ EvenOdd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int no;
+    cout << "Enter no: ";
+    cin >> no;
+
+    if(no % 2 == 0)
+        cout << "Number is even";
+    else
+        cout << "NUmber is odd";
+}
