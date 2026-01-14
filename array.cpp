67ada322ed5554b3b84c4029a3e959f140062949
Kushal Shah <124665390+KushalShah2512@users.noreply.github.com>
2026-01-15 00:27:51 +0530

    Implement basic array input and output

---
+++ array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: \n";
+    for(int i=0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements are: \n";
+    for(int i=0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+
+}
